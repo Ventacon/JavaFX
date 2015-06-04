e391d867fb3cc02bcac4639226ccebd8a98e99b4
Bogachan <bogachan.oenal@live.de>
2015-06-04 20:57:14 +0200

    neu

---
+++ JavaFX/src/main/Client.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Bogachan
+ *
+ */
+public class Client extends Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+	}
+
+}
---
+++ JavaFX/src/main/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Bogachan
+ *
+ */
+package main;
