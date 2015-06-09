e9a944656c562085a7a44f1e2f3ccda2fc47b8a8
Bogachan <bogachan.oenal@live.de>
2015-06-09 19:55:42 +0200

    neu

--- JavaFX/src/main/Client.java
+++ JavaFX/src/main/Client.java
@@ -1,6 +1,9 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 /**
@@ -13,12 +16,21 @@ public class Client extends Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		launch(args);
 
 	}
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		BorderPane pane = new BorderPane();
 
+		Scene scene = new Scene(pane);
+
+		stage.setTitle("Hier ist der Titel!");
+		stage.centerOnScreen();
+		stage.setHeight(720);
+		stage.setWidth(1020);
+		stage.setScene(scene);
+		stage.show();
 	}
-
 }
